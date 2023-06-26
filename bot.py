fd695e696064fe77190ecf59499de2a3419c943c
Jon Hultz <jhultz@redhat.com>
2023-06-26 15:42:43 -0700

    update

--- bot.py
+++ bot.py
@@ -1,16 +1,96 @@
 import os
 import streamlit as st
+import re
+import time
+
+#Modules to Import
+from io import BytesIO
+from typing import Any, Dict, List
 from llama_index import SimpleDirectoryReader
 from llama_index.node_parser import SimpleNodeParser
 from llama_index import GPTVectorStoreIndex
 from llama_index import LLMPredictor, GPTVectorStoreIndex, PromptHelper, ServiceContext, OpenAIEmbedding
 from llama_index import StorageContext, load_index_from_storage
 from langchain import OpenAI
+from langchain import LLMChain
+from langchain.agents import AgentExecutor, Tool, ZeroShotAgent
+from langchain.chains import RetrievalQA
+from langchain.chains.question_answering import load_qa_chain
+from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import VectorStore
+from langchain.vectorstores.faiss import FAISS
 
+#Define chat caching
+@st.cache_data
+def parse_pdf(file: BytesIO) -> List[str]:
+    pdf = PdfReader(file)
+    output = []
+    for page in pdf.pages:
+        text = page.extract_text()
+        # Merge hyphenated words
+        text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+        # Fix newlines in the middle of sentences
+        text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+        # Remove multiple newlines
+        text = re.sub(r"\n\s*\n", "\n\n", text)
+        output.append(text)
+    return output
+
+
+@st.cache_data
+def text_to_docs(text: str) -> List[Document]:
+    """Converts a string or list of strings to a list of Documents
+    with metadata."""
+    if isinstance(text, str):
+        # Take a single string as one page
+        text = [text]
+    page_docs = [Document(page_content=page) for page in text]
+
+    # Add page numbers as metadata
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    # Split pages into chunks
+    doc_chunks = []
+
+    for doc in page_docs:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=4000,
+            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+            chunk_overlap=0,
+        )
+        chunks = text_splitter.split_text(doc.page_content)
+        for i, chunk in enumerate(chunks):
+            doc = Document(
+                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
+            )
+            # Add sources a metadata
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
+            doc_chunks.append(doc)
+    return doc_chunks
+
+
+@st.cache_data
+def test_embed():
+    embeddings = OpenAIEmbeddings(openai_api_key=api)
+    # Indexing
+    # Save in a Vector DB
+    with st.spinner("It's indexing..."):
+        index = FAISS.from_documents(pages, embeddings)
+    st.success("Embeddings done.", icon="✅")
+    return index
+
+#Openapi Key 
 OPENAI_API_KEY = os.getenv("AI")
 if not OPENAI_API_KEY:
   raise "Env variable AI Key not specified"
 
+#static paths. TBD removal of this! 
 doc_path = '/docs/'
 index_file = 'index.pdf'
 index = None
@@ -20,7 +100,7 @@ prompt_helper = PromptHelper(context_window=4096, num_output=256, chunk_overlap_
 embed_model = OpenAIEmbedding()
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper,embed_model=embed_model)
 
-st.title("Roku Bot")
+st.title("Tiger Bot")
 
 if 'response' not in st.session_state:
     st.session_state.response = ''
@@ -30,6 +110,11 @@ def load_context():
     index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context, prompt_helper=prompt_helper)
     index.storage_context.persist(persist_dir="<persist_dir>")
     return index 
+
+if "memory" not in st.session_state:
+            st.session_state.memory = ConversationBufferMemory(
+                memory_key="chat_history"
+            )
 
 index = load_context()
 
@@ -45,3 +130,4 @@ if index != None:
     if st.session_state.response:
         st.subheader("Response: ")
         st.success(st.session_state.response, icon= "🤖")
+
